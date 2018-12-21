24f93ff55e9bae675cc8d334abdbe26d55e8e6b9
Hein Dauven <heindauven@gmail.com>
2018-12-21 18:07:48 +0100

    Add database connection setup.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
+
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
+const db = require("./util/database");
 
 const app = express();
 
@@ -10,6 +13,14 @@ app.set("view engine", "ejs");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+
+db.execute("SELECT * FROM products")
+  .then(result => {
+    console.log(result);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
