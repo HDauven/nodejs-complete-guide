f25f90dc75bc6020a1815d97d7daf56e5c959537
Hein Dauven <heindauven@gmail.com>
2018-12-24 23:46:34 +0100

    Update database connection to use mongodb

--- util/database.js
+++ util/database.js
@@ -1,10 +1,30 @@
 const { DB_HOST, DB_USER, DB_PASS } = process.env;
 
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node-complete", DB_USER, DB_PASS, {
-  host: DB_HOST,
-  dialect: "mysql"
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = callback => {
+  MongoClient.connect(
+    `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/shop?retryWrites=true`
+  )
+    .then(client => {
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+module.exports.mongoConnect = mongoConnect;
+module.exports.getDb = getDb;
