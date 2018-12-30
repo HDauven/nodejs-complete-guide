1d7c862d663da05a739824cb42ea4bad7e2bfa29
Hein Dauven <heindauven@gmail.com>
2018-12-30 17:14:29 +0100

    Disable e-mail sending on registration.

--- controllers/auth.js
+++ controllers/auth.js
@@ -205,15 +205,15 @@ exports.postReset = (req, res, next) => {
       })
       .then(result => {
         res.redirect("/");
-        transporter.sendMail({
-          to: req.body.email,
-          from: "shop@node-complete.com",
-          subject: "Password reset",
-          html: `
-            <p>You requested a password reset</p>
-            <p>Click this <a href="http://localhost:3000/reset/${token}>link</a> to set a new password.</p>
-          `
-        });
+        // transporter.sendMail({
+        //   to: req.body.email,
+        //   from: "shop@node-complete.com",
+        //   subject: "Password reset",
+        //   html: `
+        //     <p>You requested a password reset</p>
+        //     <p>Click this <a href="http://localhost:3000/reset/${token}>link</a> to set a new password.</p>
+        //   `
+        // });
       })
       .catch(err => {
         const error = new Error(err);
